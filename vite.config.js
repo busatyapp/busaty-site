4909c57b0bd2235d0fa4b793250ea1777fd894b0
EmadOmda2022 <116506191+EmadOmda2022@users.noreply.github.com>
2025-10-22 13:14:46 +0300

    Include multipage build outputs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/',
   build: {
     target: 'es2018',
     sourcemap: false,
-    minify: 'esbuild'
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        help: resolve(__dirname, 'help.html'),
+        terms: resolve(__dirname, 'terms.html')
+      }
+    }
   }
 });
